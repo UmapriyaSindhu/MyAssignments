d9b54383a2b870bbf34f718a3e493da335ede955
UmapriyaSindhu <110723423+UmapriyaSindhu@users.noreply.github.com>
2022-08-29 23:36:35 +0530

    Add files via upload

---
+++ FindSmall.java
@@ -0,0 +1,14 @@
+package week1.day2;
+import java.util.Arrays;
+public class FindSmall
+{
+	    
+		   public static void main(String[] args) 
+		  {
+			
+			int[] array1={23,45,67,32,89,22 };
+			Arrays.sort(array1);
+			System.out.println(array1[0]);
+		  }
+}
+
